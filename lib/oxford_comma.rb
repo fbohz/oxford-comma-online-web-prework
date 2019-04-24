2b5befb8047fbbd15d364d2cb0912e53eb2c5833
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-24 17:42:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,10 +6,10 @@ def oxford_comma(array)
   elsif array.length === 3
     newArray = "#{array[0]}, #{array[1]}, and #{array[2]}"
   else 
-     array.join(" , ") 
     lastArray = array.last
     array.pop
     array << "and #{lastArray}"
+    array.join(", ") 
     
   end    
 end
