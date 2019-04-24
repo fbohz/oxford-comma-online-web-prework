15da84d5bb51668d18a6d9e75e69a412c771c8f0
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-24 17:13:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.length === 2
     array.join(" and ")     
   elsif array.length === 3
-    newArray = array[0,1]
+    newArray = "#{array[0]}, #{array[1]}, and #{array[2]}"
   end    
 end
